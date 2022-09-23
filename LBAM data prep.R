75e1b8a21470a90ea5abb693602c82ece034577d
ChrisJones687 <cjones1688@gmail.com>
2022-09-23 14:27:10 -0400

    setup for plotting examples

--- LBAM data prep.R
+++ LBAM data prep.R
@@ -3,10 +3,14 @@ library(folderfun)
 library(terra)
 
 setff("In", "H:/Shared drives/Data/Original/pest-occurrence/light brown apple moth/")
-setff
+setff("states", "H:/Shared drives/Data/Vector/USA/")
 
+
+states <- vect(ffstates("us_lower_48_states.gpkg"))
+plot(states)
 LBAM_2007 <- st_read(ffIn("LBAM_2007.shp"))
 plot(LBAM_2007)
 
 ## use terra instead
 LBAM_2007 <- vect(ffIn("LBAM_2007.shp"))
+plot(LBAM_2007, add = TRUE)
